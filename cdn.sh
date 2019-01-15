e31e4bcbf2390f86778e510a958460eaeebfdc86
zkl2333 <i@zkl2333.com>
2019-01-15 22:08:33 +0800

    ci: 优化cdn脚本

--- cdn.sh
+++ cdn.sh
@@ -2,4 +2,7 @@ curl https://api.upyun.com/buckets/purge/batch \
 -H 'Authorization: Bearer '$CDN_TOKEN \
 -X POST \
 -d 'source_url=http://home.zkl2333.com/
-http://home.zkl2333.com/*'
+http://home.zkl2333.com/*' \
+-o t.txt
+cat t.txt
+rm t.txt
--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,6 @@ ip=$FTP_HOST       #remote IP
 user=$FTP_USER   #ftp username 
 password=$FTP_PASSWORD   #password 
 sss=`find $updir -path "./.git" -prune -o -type d -printf '%P\n'| awk '{if ($0 == "")next;print "mkdir " $0}'`  
-echo $sss
 aaa=`find $updir -path "./.git" -prune -o -type f -printf 'put %P\n'`  
 ftp -nv $ip <<EOF  
 user $user $password 
