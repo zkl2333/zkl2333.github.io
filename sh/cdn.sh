9140c5fdc362d60918ff1125accf99bd05dcdd0c
zkl2333 <i@zkl2333.com>
2020-01-17 23:54:04 +0800

    feat(ci): 修改刷新脚本

--- sh/cdn.sh
+++ sh/cdn.sh
@@ -3,7 +3,4 @@ curl https://api.upyun.com/buckets/purge/batch \
 -H 'Authorization: Bearer '$CDN_TOKEN \
 -X POST \
 -d 'source_url=http://home.zkl2333.com/
-http://home.zkl2333.com/*' \
--o t.txt
-echo -e '\n打印结果'
-cat t.txt
+http://home.zkl2333.com/*'
